a011c520916f6459661778f9b374bfe9ade0a5a5
David Hidalgo <juandak16@gmail.com>
2021-09-14 21:06:01 -0400

    feat: add header component

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header className='Header'>
+      <h1 className='Header-title'>
+        HACKER NEWS
+      </h1>
+    </header>
+  )
+}
